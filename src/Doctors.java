a27db6af819f4441a99460de8b6f3329793d2166
SMORc1 <marcjosephesoriano@gmail.com>
2025-08-24 19:00:51 +0800

    Doctors.java

--- src/Doctors.java
+++ src/Doctors.java
@@ -1,20 +1,35 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Doctors {
     private String name;
     private String specialization;
-    private String schedule;
+    private String scheduleDays;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
-    public Doctors(String name, String specialization, String schedule) {
+    public Doctors(String name, String specialization, String scheduleDays, LocalTime startTime, LocalTime endTime) {
         this.name = name;
         this.specialization = specialization;
-        this.schedule = schedule;
+        this.scheduleDays = scheduleDays;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     public String getName() { return name; }
     public String getSpecialization() { return specialization; }
-    public String getSchedule() { return schedule; }
+    public String getScheduleDays() { return scheduleDays; }
+    public LocalTime getStartTime() { return startTime; }
+    public LocalTime getEndTime() { return endTime; }
+
+    //Helper method which formats the time for display later
+    private String getFormattedTime(LocalTime time) {
+        return time.format(DateTimeFormatter.ofPattern("ha"));
+    }
 
     @Override
     public String toString() {
-        return name + " - " + specialization + " (" + schedule + ")";
+        return name + " - " + specialization + " (" + scheduleDays + " " + getFormattedTime(startTime) + "-" + getFormattedTime(endTime) + ")";
     }
+
 }
